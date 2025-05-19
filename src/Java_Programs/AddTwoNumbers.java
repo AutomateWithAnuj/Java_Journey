e18462613538cf08345e11299b65c97e23f32992
anujr <anujrajput8959@gmail.com>
2025-05-19 14:15:03 +0530

    Latest Push 19 May

---
+++ src/Java_Programs/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package Java_Programs;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}
