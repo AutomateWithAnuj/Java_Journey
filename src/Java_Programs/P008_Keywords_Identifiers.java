f10101b4014760fcf4e5f0aab0702f72847ec5c0
anujr <anujrajput8959@gmail.com>
2025-05-21 15:25:58 +0530

    21st May changes

---
+++ src/Java_Programs/P008_Keywords_Identifiers.java
@@ -0,0 +1,20 @@
+package Java_Programs;
+
+public class P008_Keywords_Identifiers {
+    public static void main(String[] args) {
+        //package - keywords
+        //public - keywords
+        //class - keywords
+        //static - keywords
+        //void - keywords
+        //P008_Keywords_Identifiers - Identifiers
+        //Java_Programs - Identifiers
+        //class name cannot be 123 because it is not a valid identifier
+        //ABC123 is valid identifier.
+        //_ABC_abc_ is valid identifier
+        //$Anuj is valid identifier
+        //This_is_a_very_long_name_class_blah_blah is also a valid identifier
+        //This is Anuj is not a valid identifier
+        //This$is$Anuj is a valid identifier
+    }
+}
