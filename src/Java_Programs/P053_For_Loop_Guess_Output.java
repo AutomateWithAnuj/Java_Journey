99242f4e4f8fa1b9d3585700228feaad424b8fac
anujr <anujrajput8959@gmail.com>
2025-05-28 18:39:39 +0530

    28th May 2025 changes

---
+++ src/Java_Programs/P053_For_Loop_Guess_Output.java
@@ -0,0 +1,14 @@
+package Java_Programs;
+
+public class P053_For_Loop_Guess_Output {
+    public static void main(String[] args) {
+        for (int i=0;i<18;i++){
+            if(i>15){
+                System.out.println("Gift from Papa, IPhone");
+            }
+            else{
+                System.out.println("No Gift");
+            }
+        }
+    }
+}
