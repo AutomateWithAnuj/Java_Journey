e066a559131bbfae1931321982ffa0c65f7636d3
anujr <anujrajput8959@gmail.com>
2025-06-03 23:56:10 +0530

    3rd June Changes

---
+++ src/Java_Programs/P114_Find_Max_In_Array.java
@@ -0,0 +1,17 @@
+package Java_Programs;
+
+public class P114_Find_Max_In_Array {
+    public static void main(String[] args) {
+        //do without shorting
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int max = matrix[0][0];
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                if(max<matrix[i][j]){
+                    max=matrix[i][j];
+                }
+            }
+        }
+        System.out.println("Max is: "+max);
+    }
+}
--- src/Java_Programs/P115_Reverse_Right_Angle_Triangle.java
+++ src/Java_Programs/P115_Reverse_Right_Angle_Triangle.java
@@ -2,21 +2,18 @@ package Java_Programs;
 
 import java.util.Scanner;
 
-public class P077_RightTriangle_Pyramid {
+public class P115_Reverse_Right_Angle_Triangle {
     public static void main(String[] args) {
+        //****
+        //***
+        //**
+        //*
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        //*
-        //**
-        //***
-        //****
-        //*****
         for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                if(i>=j){
-                    System.out.print("*");
-                }
+            for (int j=0;j<n-i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
