561189a5d2a0c24c205133734121a9b9b3eb768d
anujr <anujrajput8959@gmail.com>
2025-07-11 13:38:01 +0530

    11th July Changes

---
+++ src/Java_Programs/_39_JSON/JsonValueFinding/CheckingValues.java
@@ -0,0 +1,10 @@
+package Java_Programs._39_JSON.JsonValueFinding;
+
+public class CheckingValues {
+    public static void main(String[] args) {
+        //get the first person age
+        //members[0].age; //this is the way to get the first person age
+        //second person first power
+        //members[1].powers[0];
+    }
+}
