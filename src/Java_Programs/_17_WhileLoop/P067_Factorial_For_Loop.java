ca237d3e2dfa35dcd34cc09f7178393cea5b2d96
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 21:24:21 +0530

    Add comment to explain factorial calculation
    Added a comment to clarify the purpose of the loop.

--- src/Java_Programs/_17_WhileLoop/P067_Factorial_For_Loop.java
+++ src/Java_Programs/_17_WhileLoop/P067_Factorial_For_Loop.java
@@ -6,6 +6,7 @@ public class P067_Factorial_For_Loop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        //mutiply all the numbers from 1 to n
         int n = sc.nextInt();
         int f = 1;
         for (int i=n;i>=1;i--){
