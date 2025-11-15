597072084fe07dee2b8d618d6585cdc4c54e7eb7
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 15:20:44 +0530

    Update P052_For_Loop_Guess_Output.java

--- src/Java_Programs/_16_ForLoop/P052_For_Loop_Guess_Output.java
+++ src/Java_Programs/_16_ForLoop/P052_For_Loop_Guess_Output.java
@@ -2,7 +2,7 @@ package Java_Programs._16_ForLoop;
 
 public class P052_For_Loop_Guess_Output {
     public static void main(String[] args) {
-        for (int i=0;i>1;i++){
+        for (int i=0;i>1;i++){ //if condition become false the loop will be broken and will not execute any furthur
             System.out.println(i);
         }
         System.out.println("End"); //only this will be printed
