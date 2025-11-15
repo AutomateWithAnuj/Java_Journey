98442f0bc0224d95e84016f4856814d03456d969
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 15:24:09 +0530

    Update P053_For_Loop_Guess_Output.java

--- src/Java_Programs/_16_ForLoop/P053_For_Loop_Guess_Output.java
+++ src/Java_Programs/_16_ForLoop/P053_For_Loop_Guess_Output.java
@@ -5,9 +5,11 @@ public class P053_For_Loop_Guess_Output {
         for (int i=0;i<18;i++){
             if(i>15){
                 System.out.println("Gift from Papa, IPhone");
+                //after 15 years of age papa will gift iPhones
             }
             else{
                 System.out.println("No Gift");
+                //this the son turns 16 papa will not give any gift to him
             }
         }
     }
