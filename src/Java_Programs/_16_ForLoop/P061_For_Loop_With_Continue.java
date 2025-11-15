72f27c0a5df345c3e1f1e795d13a188293a90f76
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 16:15:40 +0530

    Update comments in For Loop with Continue example
    Clarified comments regarding the continue statement in the loop.

--- src/Java_Programs/_16_ForLoop/P061_For_Loop_With_Continue.java
+++ src/Java_Programs/_16_ForLoop/P061_For_Loop_With_Continue.java
@@ -6,10 +6,11 @@ public class P061_For_Loop_With_Continue {
         for (int i=0;i<50;i++){
             if(i==5){
                 continue;
-            }
-            System.out.println(i); //except 5 it will print everything
-            //so in case of 5 it will move out of that and execute by 6
+            //except 5, it will print everything
+            //so in case of 5, it will move out of that if block and execute by 6
             //System.out.println(i) will not execute for the i==5
+            }
+            System.out.println(i); 
         }
     }
 }
