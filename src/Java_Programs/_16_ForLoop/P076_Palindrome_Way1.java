e2056c688e0b98ee93609be25cc4b498d9fefb06
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 16:50:56 +0530

    Refactor palindrome check for clarity

--- src/Java_Programs/_16_ForLoop/P076_Palindrome_Way1.java
+++ src/Java_Programs/_16_ForLoop/P076_Palindrome_Way1.java
@@ -8,12 +8,12 @@ public class P076_Palindrome_Way1 {
         System.out.print("Enter Text: ");
         int n = 0;
         String s = sc.nextLine();
-        for (int i=0;i<s.length()/2;i++){
+        for (int i=0;i<s.length()/2;i++){ //checking till half characters starting from 0th index
             if(s.charAt(i)==s.charAt(s.length()-1-i)){
-                n++;
+                n++; //n is the number of characters that become equal
             }
         }
-        if(n==s.length()/2){
+        if(n==s.length()/2){ //n must be equal to the half characters, then we can say it's a palindrome
             System.out.println("Yes it is a palindrome");
         } else {
             System.out.println("no it's not a palindrome");
