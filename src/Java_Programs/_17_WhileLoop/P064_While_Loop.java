3745c59f0c750076ae48d25888b199bd1203fc16
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 21:22:24 +0530

    Uncomment print statement in while loop
    Uncommented the print statement to display numbers.

--- src/Java_Programs/_17_WhileLoop/P064_While_Loop.java
+++ src/Java_Programs/_17_WhileLoop/P064_While_Loop.java
@@ -5,7 +5,7 @@ public class P064_While_Loop {
         int i=0; // Initialization
         while (i<10){ //Condition
 //            System.out.println("Start the program");
-            System.out.print(i+" ");
+            System.out.print(i+" "); //1 2 3 4 5 6 7 8 9
             i++; //Updating
         }
     }
