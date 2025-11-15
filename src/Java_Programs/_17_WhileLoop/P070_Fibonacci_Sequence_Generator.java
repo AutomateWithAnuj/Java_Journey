539f270cb06fb7ddbdacf2fcc1bb9f92f66f0938
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 22:18:56 +0530

    Modify Fibonacci generator to start from 0 and 1
    Updated Fibonacci sequence generator to start from 0 and 1.

--- src/Java_Programs/_17_WhileLoop/P070_Fibonacci_Sequence_Generator.java
+++ src/Java_Programs/_17_WhileLoop/P070_Fibonacci_Sequence_Generator.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 
 public class P070_Fibonacci_Sequence_Generator {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 1;
+        // 0 1 1 2 3 5 8 13 21 34
+
+        int a = 0;   // first number
+        int b = 1;   // second number
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter how many values you want: ");
         int NoOfvalues = sc.nextInt();
+
         int n = 1;
-        String s = "";
-        while (n<=NoOfvalues){
-            if(n==1){
-                System.out.print("1, ");
-            } else if (n==2) {
-                System.out.print("1, ");
+
+        while (n <= NoOfvalues) {
+            if (n == 1) {
+                System.out.print(a + ", ");  // print 0
+            } else if (n == 2) {
+                System.out.print(b + ", ");  // print 1
             } else {
                 int temp = b;
-                b = b+a;
-                a = temp;
-                System.out.printf("%d, ",b);
+                b = a + b;   // this is what the next Fibonacci value, which b must take
+                a = temp; //don't change b now only replace a with old b
+                System.out.print(b + ", ");
             }
             n++;
         }
