e96b2badd32e7bbd346c1186df691663f3a954b9
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 14:05:51 +0530

    Update P045_Valid_Invalid_Switch.java

--- src/Java_Programs/_15_SwitchCase/P045_Valid_Invalid_Switch.java
+++ src/Java_Programs/_15_SwitchCase/P045_Valid_Invalid_Switch.java
@@ -4,17 +4,17 @@ public class P045_Valid_Invalid_Switch {
     public static void main(String[] args) {
         int a = 10;
         switch(a){
-            //this is valid syntax
+            //this is valid syntax because a is int and it will go inside this block
         }
 
         char ch = 'A';
         switch(a){
-            //this is valid syntax
+            //this is valid syntax because a is a char and it will go inside this block
         }
 
         boolean b = true;
         /*switch(b){
-            //this is not valid syntax because switch cannot take boolean
+            //this is not valid syntax because a switch cannot take a boolean
         }*/
 
         long all = 30l;
@@ -22,6 +22,7 @@ public class P045_Valid_Invalid_Switch {
             //this is a valid syntax
         }
 
+        //is 2 duplicate cases are allowed
         int i = 98;
         switch (i){
             case 98:
