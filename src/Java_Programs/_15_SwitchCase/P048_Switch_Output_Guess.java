777a5cb329198a0d63291b3434f43c7985af39de
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 14:18:12 +0530

    Update P048_Switch_Output_Guess.java

--- src/Java_Programs/_15_SwitchCase/P048_Switch_Output_Guess.java
+++ src/Java_Programs/_15_SwitchCase/P048_Switch_Output_Guess.java
@@ -5,7 +5,7 @@ public class P048_Switch_Output_Guess {
         int a = 11;
         switch(-2){
             default:
-                //it will only go in default if there is no match case for it
+                //it will only go in default if there is no match case for it else not
                 //even if it's on top it's position doesn't matter
                 System.out.println("Default");
                 break;
@@ -14,8 +14,8 @@ public class P048_Switch_Output_Guess {
                 System.out.println("10");
                 break;
             case 11:
-                System.out.println("11");
-                break;
+                System.out.println("11"); // it wil go inside this
+                break; // and break it here
         }
     }
 }
