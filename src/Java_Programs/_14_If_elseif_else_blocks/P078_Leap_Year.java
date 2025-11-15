641596b156cf1c74af9b3b94c5a25512efaabac7
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 13:44:10 +0530

    Clarify year variable size in Leap Year program
    Added comment to clarify the size of the year variable.

--- src/Java_Programs/_14_If_elseif_else_blocks/P078_Leap_Year.java
+++ src/Java_Programs/_14_If_elseif_else_blocks/P078_Leap_Year.java
@@ -6,7 +6,7 @@ public class P078_Leap_Year {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year: ");
-        int year = sc.nextInt();
+        int year = sc.nextInt(); //32 bit size //-2,147,483,648  to  2,147,483,647
         if(year%100==0&&(year/100)%4==0){
             System.out.println("Yes it is a leap year");
         }else if (year%100!=0&&year%4==0){
