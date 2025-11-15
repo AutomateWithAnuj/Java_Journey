7a21898beded3fd7b81af081865737b74ed50b91
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-15 16:07:19 +0530

    Update comments in P056_For_Loop_Outside_ICU.java
    Corrected comments for clarity and fixed formatting.

--- src/Java_Programs/_16_ForLoop/P056_For_Loop_Outside_ICU.java
+++ src/Java_Programs/_16_ForLoop/P056_For_Loop_Outside_ICU.java
@@ -2,10 +2,10 @@ package Java_Programs._16_ForLoop;
 
 public class P056_For_Loop_Outside_ICU {
     public static void main(String[] args) {
-        //yes outside initializtion is possible - for loop
+        //yes outside initialisation is possible - for loop
         int i = 10; //initialization can be done outside also
-        //boolean c = i>0; //it will not work if we will use c in for loop so Condition cannot be given
-        //Updating also cannot be done outside for loop
+        //boolean c = i>0; //it will not work if we use c in a for loop as a condition, because outside the loop, it cannot check every time 
+        //Updating also cannot be done outside for loop; we have to do it  within the loop
         for(;i>10;i--){ //only initialization can be done outside nothing else
             System.out.println(i);
         }
