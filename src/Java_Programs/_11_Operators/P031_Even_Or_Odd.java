b79f33b067096e712f23ef9ee793d6a79d29bd64
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-23 17:23:31 +0530

    Update P031_Even_Or_Odd.java

--- src/Java_Programs/_11_Operators/P031_Even_Or_Odd.java
+++ src/Java_Programs/_11_Operators/P031_Even_Or_Odd.java
@@ -2,8 +2,14 @@ package Java_Programs._11_Operators;
 
 public class P031_Even_Or_Odd {
     public static void main(String[] args) {
-        int num = 10;
-        String res = num%2==0 ? "Even":"Odd";
-        System.out.println(res);
+
+        int num = 10;  
+        // We are checking if the number is even or odd
+
+        // Ternary operator:
+        // If num % 2 == 0 → "Even", otherwise → "Odd"
+        String res = (num % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(res);   // Output: Even
     }
 }
