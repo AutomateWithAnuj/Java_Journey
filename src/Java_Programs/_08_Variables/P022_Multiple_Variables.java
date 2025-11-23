4dd0bf64f2d1506a35192b397b522ea244a70795
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-23 15:22:54 +0530

    Update P022_Multiple_Variables.java

--- src/Java_Programs/_08_Variables/P022_Multiple_Variables.java
+++ src/Java_Programs/_08_Variables/P022_Multiple_Variables.java
@@ -2,6 +2,6 @@ package Java_Programs._08_Variables;
 
 public class P022_Multiple_Variables {
     public static void main(String[] args) {
-        int a = 10, b = 23, c = 90;
+        int a = 10, b = 23, c = 90; //here we are declaring and initialising 3 variables in a single line
     }
 }
