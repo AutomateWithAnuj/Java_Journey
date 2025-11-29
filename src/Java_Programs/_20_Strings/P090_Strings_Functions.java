0335df740573670a79e9ecbadc2517055b63cc39
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-11-29 22:08:27 +0530

    Update P090_Strings_Functions.java

--- src/Java_Programs/_20_Strings/P090_Strings_Functions.java
+++ src/Java_Programs/_20_Strings/P090_Strings_Functions.java
@@ -2,10 +2,19 @@ package Java_Programs._20_Strings;
 
 public class P090_Strings_Functions {
     public static void main(String[] args) {
+        
         String s1 = "ABC";
-        System.out.println(s1.length());//3
-        System.out.println(s1.toLowerCase());
-        System.out.println(s1.toUpperCase());
-        System.out.println(s1.concat("D"));
+        
+        // length(): returns the total number of characters in the string
+        System.out.println(s1.length());   // Output: 3
+        
+        // toLowerCase(): converts the string to all lowercase letters
+        System.out.println(s1.toLowerCase());  // Output: abc
+        
+        // toUpperCase(): converts the string to all uppercase letters
+        System.out.println(s1.toUpperCase());  // Output: ABC
+        
+        // concat(): joins/append another string to the existing string 
+        System.out.println(s1.concat("D"));    // Output: ABCD
     }
 }
