0a9c85e222d19b8ca33d2544ad986d4d4591b07c
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-07 22:51:19 +0530

    Update P098_Split_Function.java

--- src/Java_Programs/_20_Strings/P098_Split_Function.java
+++ src/Java_Programs/_20_Strings/P098_Split_Function.java
@@ -2,10 +2,25 @@ package Java_Programs._20_Strings;
 
 public class P098_Split_Function {
     public static void main(String[] args) {
+
+        // A simple string
         String str = "anuj";
+
+        // split("") means split on every character
+        // So the string will break into an array of single characters:
+        // ["a", "n", "u", "j"]
         String[] parts = str.split("");
-        for (String part: parts){
+
+        // Using enhanced for loop to print each character separately
+        for (String part : parts){
             System.out.println(part);
+            /*
+                Output:
+                a
+                n
+                u
+                j
+            */
         }
     }
 }
