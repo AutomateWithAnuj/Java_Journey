086c9522ab47edd9805e54c3b965f44a362e5f2e
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-07 22:06:39 +0530

    Update P095_StringBuilder.java

--- src/Java_Programs/_20_Strings/P095_StringBuilder.java
+++ src/Java_Programs/_20_Strings/P095_StringBuilder.java
@@ -2,11 +2,30 @@ package Java_Programs._20_Strings;
 
 public class P095_StringBuilder {
     public static void main(String[] args) {
+
         StringBuilder stringBuilder = new StringBuilder("Anuj");
-        //Similar to the String buffer we can use string builder
+        // Creates a mutable StringBuilder object in Heap memory.
+        // Initial value: "Anuj"
+
         System.out.println(stringBuilder.append("Rajput"));
-        // here append also allow us to use different datatypes
+        /* 
+         Output: AnujRajput
+         Reason: append() modifies the same object instead of creating a new one.
+         There is no need to assign back because StringBuilder is mutable.
+        */
+
         System.out.println(stringBuilder.append(123));
+        /*
+          Output: AnujRajput123
+          Reason: append() supports multiple data types 
+          (int, boolean, float, char, string, etc.), and appends them directly.
+        */
+
         System.out.println(stringBuilder.reverse());
+        /*
+          Output: 321tupjaRjunA
+          Reason: reverse() reverses the characters of the current content 
+          in the same object (still one object in memory).
+        */
     }
 }
