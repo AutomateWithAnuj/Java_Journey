29ddde3c1f32b3c2514c3864881699ec512d552d
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-08 19:20:29 +0530

    Update P114_Find_Max_In_Array.java

--- src/Java_Programs/_21_Arrays/P114_Find_Max_In_Array.java
+++ src/Java_Programs/_21_Arrays/P114_Find_Max_In_Array.java
@@ -1,17 +1,43 @@
 package Java_Programs._21_Arrays;
 
 public class P114_Find_Max_In_Array {
+
     public static void main(String[] args) {
-        //do without shorting
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+
+        // Example Matrix (3x3)
+        //  1   2   3
+        //  4   5   6
+        //  7   8   9
+        //
+        // We will iterate through each element and find the maximum value
+        // WITHOUT using sorting.
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        // Assuming first element as max initially
         int max = matrix[0][0];
-        for (int i=0;i<matrix.length;i++){
-            for (int j=0;j<matrix[i].length;j++){
-                if(max<matrix[i][j]){
-                    max=matrix[i][j];
+
+        // Loop to find the maximum element
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
                 }
             }
         }
-        System.out.println("Max is: "+max);
+
+        System.out.println("Maximum Value in Matrix is: " + max);
+
+        /*
+         -------- Example Output --------
+
+         Maximum Value in Matrix is: 9
+
+         --------------------------------
+        */
     }
 }
