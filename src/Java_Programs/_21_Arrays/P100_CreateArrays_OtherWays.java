049cc1c39bf0ce266b9ae04a2fe3617ce9998c0b
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-08 12:46:02 +0530

    Refactor array creation and improve comments
    Updated comments for clarity and corrected name assignment.

--- src/Java_Programs/_21_Arrays/P100_CreateArrays_OtherWays.java
+++ src/Java_Programs/_21_Arrays/P100_CreateArrays_OtherWays.java
@@ -2,17 +2,39 @@ package Java_Programs._21_Arrays;
 
 public class P100_CreateArrays_OtherWays {
     public static void main(String[] args) {
-        int[] marks2 = new int[5];
+
+        // Creating an empty int array of size 5
+        int[] marks2 = new int[5];  // All values will automatically be initialized to 0
+
+        // Another valid way of declaring an array (same as above)
         int marks3[] = new int[5];
-        //these both are valid
+
+        // Creating a String array with size 3
         String[] names = new String[3];
+
+        // Assigning values using index
         names[0] = "anuj";
         names[1] = "amit";
-        names[2] = "goutam";
+        names[2] = "lekhu";
 
-        System.out.println(marks2[0]);//it will give us the address of that array
-        for (int i=0;i<names.length;i++){
+        // Printing first element of marks2
+        System.out.println(marks2[0]);
+        /*
+            Output: 0
+            Reason: For numeric arrays, Java assigns a default value = 0 
+            (not an address, because printing an array element prints its stored value)
+        */
+
+        // Printing each name using a loop
+        for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
+            /*
+                Output:
+                anuj
+                amit
+                lekhu
+                Reason: We stored these values using indexes.
+            */
         }
     }
 }
