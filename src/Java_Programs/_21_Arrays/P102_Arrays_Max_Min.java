828fe4c71d0313e35b7e571d61910c479cb6514f
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-08 14:54:10 +0530

    Enhance comments and formatting in max/min array program
    Added comments for clarity and improved code readability.

--- src/Java_Programs/_21_Arrays/P102_Arrays_Max_Min.java
+++ src/Java_Programs/_21_Arrays/P102_Arrays_Max_Min.java
@@ -2,19 +2,25 @@ package Java_Programs._21_Arrays;
 
 public class P102_Arrays_Max_Min {
     public static void main(String[] args) {
+        // in this, we are trying to find out the max and min inside the array
         int[] array = {25,14,56,15,36,56,77,18,29,49};
-        //Can we take the input from the user also for Arrays? Yes
-        //Max & Min
+
+        // Can we take input from the user for arrays? Yes, by using the Scanner class.
+
+        // Initial values for Max and Min are set to the first element of the array
         int Max = array[0];
         int Min = array[0];
-        for (int i=0;i<array.length;i++){
-            if(array[i]>Max){
-                Max=array[i];
-            } else if (array[i]<Min) {
-                Min=array[i];
+
+        // Loop through the array to compare each element
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > Max) {
+                Max = array[i];
+            } else if (array[i] < Min) {
+                Min = array[i];
             }
         }
-        System.out.println("Max value is: "+Max);
-        System.out.println("Min value is: "+Min);
+
+        System.out.println("Max value is: " + Max);
+        System.out.println("Min value is: " + Min);
     }
 }
