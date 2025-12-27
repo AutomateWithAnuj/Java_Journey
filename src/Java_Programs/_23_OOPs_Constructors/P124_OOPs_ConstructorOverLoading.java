77c289b947f14194e081109ee785e51dcd4f9dbe
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 16:52:56 +0530

    Enhance Car3 class with overloaded constructors
    Refactor Car3 class to include constructors with different parameters and improve comments.

--- src/Java_Programs/_23_OOPs_Constructors/P124_OOPs_ConstructorOverLoading.java
+++ src/Java_Programs/_23_OOPs_Constructors/P124_OOPs_ConstructorOverLoading.java
@@ -1,48 +1,79 @@
 package Java_Programs._23_OOPs_Constructors;
 
-public class P124_OOPs_ConstructorOverLoading {
-    public static void main(String[] args) {
-        Car3 c = new Car3();
-        Car3 c2 = new Car3("Anshul",2025,"i20"); //you can also provide details here
-        System.out.println(c.name);
-        System.out.println(c.year);
-        System.out.println(c.model);
-        System.out.println(c2.name);
-        System.out.println(c2.year);
-        System.out.println(c2.model);
-        Car3 c3 = new Car3("Amit",2025); //you can also provide details here
-        System.out.println(c3.name);
-        System.out.println(c3.year);
-        System.out.println(c3.model);
+// Car3 class
+class Car3 {
 
-        c.running();
-        c2.running();
-        c3.running();
-    }
-}
-
-class Car3{
+    // Instance variables
     String name;
     int year;
     String model;
-    Car3(String name,int year ,String model){
-        this.name = name; // here this is c3 which is the reference of the object it has
-        this.year = year; // here this is c3 which is the reference of the object it has
-        this.model = model; // here this is c3 which is the reference of the object it has
+
+    // Constructor with 3 parameters
+    Car3(String name, int year, String model) {
+
+        // 'this' refers to the current object
+        this.name = name;
+        this.year = year;
+        this.model = model;
     }
-    Car3(String name,int year){
-        this.name = name; // here this is c3 which is the reference of the object it has
-        this.year = year; // here this is c3 which is the reference of the object it has
+
+    // Constructor with 2 parameters
+    Car3(String name, int year) {
+
+        // model is not set here, so it remains null
+        this.name = name;
+        this.year = year;
     }
-    Car3(){
+
+    // Default constructor (no parameters)
+    Car3() {
+
+        // Assigning default values manually
         name = "Anuj";
         year = 2024;
         model = "xyz";
     }
 
-    void running(){
-        System.out.println(this.name+" is Running");
-    }
+    // Instance method
+    void running() {
 
+        // 'this.name' ensures correct object name is used
+        System.out.println(this.name + " is Running");
+    }
+}
+
+public class P124_OOPs_ConstructorOverLoading {
+
+    public static void main(String[] args) {
+
+        // 1️⃣ Object created using default constructor
+        Car3 c = new Car3();
+
+        // 2️⃣ Object created using parameterised constructor (3 parameters)
+        Car3 c2 = new Car3("Anshul", 2025, "i20");
+
+        // Printing values of the first object
+        System.out.println(c.name);   // Anuj
+        System.out.println(c.year);   // 2024
+        System.out.println(c.model);  // xyz
+
+        // Printing values of the second object
+        System.out.println(c2.name);  // Anshul
+        System.out.println(c2.year);  // 2025
+        System.out.println(c2.model); // i20
+
+        // 3️⃣ Object created using parameterised constructor (2 parameters)
+        Car3 c3 = new Car3("Amit", 2025);
+
+        // Printing values of the third object
+        System.out.println(c3.name);  // Amit
+        System.out.println(c3.year);  // 2025
+        System.out.println(c3.model); // null (not initialized)
+
+        // Calling method for all objects
+        c.running();
+        c2.running();
+        c3.running();
+    }
 }
 
