26bb7ccabf976729a940c25bbd32efd688fc3b0f
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 21:59:21 +0530

    Refactor hierarchical inheritance example

--- src/Java_Programs/_24_OOPs_Inheritance/HierarchicalInheritance/P132_Hierarchical.java
+++ src/Java_Programs/_24_OOPs_Inheritance/HierarchicalInheritance/P132_Hierarchical.java
@@ -1,35 +1,48 @@
 package Java_Programs._24_OOPs_Inheritance.HierarchicalInheritance;
 
+// Base class
+class Father {
+    void h1() {
+        System.out.println("Father - 3BHK House");
+    }
+}
+
+// Child classes
+class Anuj extends Father {
+    void h2() {
+        System.out.println("Anuj - Own House");
+    }
+}
+
+class Princy extends Father {
+    void h3() {
+        System.out.println("Princy - Own House");
+    }
+}
+
+class Bhanu extends Father {
+    void h4() {
+        System.out.println("Bhanu - Own House");
+    }
+}
+
 public class P132_Hierarchical {
     public static void main(String[] args) {
+
         Father f1 = new Father();
-        Princy p1 = new Princy();
         Anuj a1 = new Anuj();
+        Princy p1 = new Princy();
         Bhanu b1 = new Bhanu();
-        p1.h1();
+
+        // Common property from Father
         f1.h1();
         a1.h1();
+        p1.h1();
         b1.h1();
+
+        // Individual properties
         a1.h2();
-    }
-}
-class Father{
-    void h1(){
-        System.out.println("3BHK");
-    }
-}
-class Anuj extends Father{
-    void h2(){
-        System.out.println("Anuj - House");
-    }
-}
-class Princy extends Father{
-    void h3(){
-        System.out.println("Princy - House");
-    }
-}
-class Bhanu extends Father{
-    void h4(){
-        System.out.println("Bhanu - House");
+        p1.h3();
+        b1.h4();
     }
 }
