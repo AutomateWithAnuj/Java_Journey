cffbcd525506343f5d38525be9d32386e8dc6a61
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 18:02:27 +0530

    Update P128_TestCase1.java

--- src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P128_TestCase1.java
+++ src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P128_TestCase1.java
@@ -1,10 +1,18 @@
 package Java_Programs._24_OOPs_Inheritance.SingleInheritance;
 
-public class P128_TestCase1 extends P127_CommonToAll_Inheritance{
-    void runningTC1(){
-        startBrowser();// One of the behaviour inherited from SuperClass
-        //hypothetically they are also present here
-        System.out.println("Running TC1");// One of the behaviour inherited from SuperClass
-        closeBrowser();//hypothetically they are also present here
+// Test case class that inherits common functionalities
+public class P128_TestCase1 extends P127_CommonToAll_Inheritance {
+
+    // Method to execute Test Case 1
+    void runTestCase1() {
+
+        // startBrowser() is inherited from parent class
+        startBrowser();
+
+        // Test case specific logic
+        System.out.println("Running Test Case 1");
+
+        // closeBrowser() is inherited from parent class
+        closeBrowser();
     }
 }
