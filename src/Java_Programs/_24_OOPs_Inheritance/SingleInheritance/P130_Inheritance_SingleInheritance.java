12efd12976936163218891fcfd24aa7f4eb68526
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 18:04:37 +0530

    Rename methods for test case execution

--- src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P130_Inheritance_SingleInheritance.java
+++ src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P130_Inheritance_SingleInheritance.java
@@ -1,10 +1,15 @@
 package Java_Programs._24_OOPs_Inheritance.SingleInheritance;
 
 public class P130_Inheritance_SingleInheritance {
+
     public static void main(String[] args) {
+
+        // Creating object of Test Case 1
         P128_TestCase1 tc1 = new P128_TestCase1();
-        tc1.runningTC1();
+        tc1.runTestCase1();   // Execute Test Case 1
+
+        // Creating object of Test Case 2
         P129_TestCase2 tc2 = new P129_TestCase2();
-        tc2.runningTC2();
+        tc2.runTestCase2();   // Execute Test Case 2
     }
 }
