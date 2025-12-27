9ab15a8894e7664aa41d096a9f014ac09798176f
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 16:48:06 +0530

    Update P123_OOPs_ParamConstructor.java

--- src/Java_Programs/_23_OOPs_Constructors/P123_OOPs_ParamConstructor.java
+++ src/Java_Programs/_23_OOPs_Constructors/P123_OOPs_ParamConstructor.java
@@ -1,22 +1,38 @@
 package Java_Programs._23_OOPs_Constructors;
 
-public class P123_OOPs_ParamConstructor {
-    public static void main(String[] args) {
-        Car2 c2 = new Car2("Anuj",2025,"i20");
-        System.out.println(c2.name);//it will give us null if no constructor
-        System.out.println(c2.year); //it will give us 0 if no constructor
-        System.out.println(c2.model); //it will give us null if no constructor
+// Car2 class
+class Car2 {
 
-    }
-}
-
-class Car2{
+    // Instance variables
     String name;
     int year;
     String model;
-    Car2(String name,int year ,String model){
+
+    //Parameterised constructor
+    Car2(String name, int year, String model) {
+
+        // 'this' refers to the current object
+        // Left side  → instance variables
+        // Right side → constructor parameters
+
         this.name = name;
         this.year = year;
         this.model = model;
+    }
+}
+
+public class P123_OOPs_ParamConstructor {
+
+    public static void main(String[] args) {
+
+        // Creating an object using a parameterised constructor
+        // Values are passed at the time of object creation
+        Car2 c2 = new Car2("Anuj", 2025, "i20");
+
+        // Printing instance variable values
+        // These values come from the constructor arguments
+        System.out.println(c2.name);   // Anuj
+        System.out.println(c2.year);   // 2025
+        System.out.println(c2.model);  // i20
     }
 }
