dbe44072c7c60cb0f41db250d3d2e75bb9b7d994
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 18:04:02 +0530

    Update P129_TestCase2.java

--- src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P129_TestCase2.java
+++ src/Java_Programs/_24_OOPs_Inheritance/SingleInheritance/P129_TestCase2.java
@@ -1,11 +1,22 @@
 package Java_Programs._24_OOPs_Inheritance.SingleInheritance;
 
+// Test case class that inherits common functionalities
 public class P129_TestCase2 extends P127_CommonToAll_Inheritance {
-    void runningTC2(){
-        startBrowser();//hypothetically they are also present here
-        System.out.println("Running TC2");
+
+    // Method to execute Test Case 2
+    void runTestCase2() {
+
+        // Inherited method from parent class
+        startBrowser();
+
+        // Test case specific logic
+        System.out.println("Running Test Case 2");
+
+        // Using inherited utility methods
         readDatabaseFile();
         readExcelFile();
-        closeBrowser();//hypothetically they are also present here
+
+        // Inherited method from parent class
+        closeBrowser();
     }
 }
