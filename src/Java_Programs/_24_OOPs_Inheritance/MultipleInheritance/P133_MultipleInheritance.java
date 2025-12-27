0648cd6ffbfc5b09557b4d98bf2bcd687f4576d7
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-27 18:17:07 +0530

    Update P133_MultipleInheritance.java

--- src/Java_Programs/_24_OOPs_Inheritance/MultipleInheritance/P133_MultipleInheritance.java
+++ src/Java_Programs/_24_OOPs_Inheritance/MultipleInheritance/P133_MultipleInheritance.java
@@ -1,7 +1,19 @@
 package Java_Programs._24_OOPs_Inheritance.MultipleInheritance;
 
 public class P133_MultipleInheritance {
+
     public static void main(String[] args) {
-        //Not possible in java
+
+        // Multiple inheritance using classes is NOT allowed in Java
+        // A class cannot extend more than one class
+
+        // ❌ This is NOT allowed:
+        // class Child extends Parent1, Parent2 { }
+
+        System.out.println("Multiple inheritance using classes is not supported in Java.");
+        System.out.println("This avoids ambiguity and the Diamond Problem.");
+
+        // ✅ Solution: Use interfaces
+        System.out.println("Java supports multiple inheritance using interfaces.");
     }
 }
