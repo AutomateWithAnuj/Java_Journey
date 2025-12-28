29a8eb18edbddafa4423eb1bd9984a36bd1e28a2
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-28 18:28:17 +0530

    Update P134_Poly_MethodOverloading.java

--- src/Java_Programs/_25_OOPs_Polymorphism/MethodOverloading/P134_Poly_MethodOverloading.java
+++ src/Java_Programs/_25_OOPs_Polymorphism/MethodOverloading/P134_Poly_MethodOverloading.java
@@ -1,24 +1,5 @@
 package Java_Programs._25_OOPs_Polymorphism.MethodOverloading;
 
-public class P134_Poly_MethodOverloading {
-
-    public static void main(String[] args) {
-
-        // Creating object of MathOperations class
-        MathOperations math = new MathOperations();
-
-        // Calling overloaded add() methods
-        int sum1 = math.add(2, 3);              // calls add(int, int)
-        float sum2 = math.add(3.7f, 5.9f);      // calls add(float, float)
-        int sum3 = math.add(2, 3, 4);            // calls add(int, int, int)
-
-        // Printing results
-        System.out.println("Sum of 2 integers        : " + sum1);
-        System.out.println("Sum of 2 float numbers   : " + sum2);
-        System.out.println("Sum of 3 integers        : " + sum3);
-    }
-}
-
 /*
  * This class demonstrates Method Overloading
  * - Same method name: add()
@@ -40,5 +21,24 @@ class MathOperations {
     // Adds three integers
     int add(int a, int b, int c) {
         return a + b + c;
+    }
+}
+
+public class P134_Poly_MethodOverloading {
+
+    public static void main(String[] args) {
+
+        // Creating object of MathOperations class
+        MathOperations math = new MathOperations();
+
+        // Calling overloaded add() methods
+        int sum1 = math.add(2, 3);              // calls add(int, int)
+        float sum2 = math.add(3.7f, 5.9f);      // calls add(float, float)
+        int sum3 = math.add(2, 3, 4);            // calls add(int, int, int)
+
+        // Printing results
+        System.out.println("Sum of 2 integers        : " + sum1);
+        System.out.println("Sum of 2 float numbers   : " + sum2);
+        System.out.println("Sum of 3 integers        : " + sum3);
     }
 }
