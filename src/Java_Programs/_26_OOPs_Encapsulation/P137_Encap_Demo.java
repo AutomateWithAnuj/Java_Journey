e88d31f17fa3b2cde09cae4a81ba04941db22f11
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-29 15:57:50 +0530

    Update P137_Encap_Demo.java

--- src/Java_Programs/_26_OOPs_Encapsulation/P137_Encap_Demo.java
+++ src/Java_Programs/_26_OOPs_Encapsulation/P137_Encap_Demo.java
@@ -1,31 +1,6 @@
 package Java_Programs._26_OOPs_Encapsulation;
 
-public class P137_Encap_Demo {
-    public static void main(String[] args) {
-
-        // Creating object of secure login class
-        GoodVWOLogin vwoL = new GoodVWOLogin("admin", "pass123");
-
-        // ❌ Direct access NOT allowed (password is private)
-        // System.out.println(vwoL.password);
-
-        // ✔ Username can be changed via setter
-        vwoL.setUsername("Anuj");
-        System.out.println(vwoL.getUsername());
-
-        // ✔ Password can be read ONLY using getter
-        System.out.println(vwoL.getPassword());
-
-        // ✔ Only admin can change password
-        vwoL.setPassword("12345", true);
-        System.out.println(vwoL.getPassword());
-
-        // ❌ Non-admin cannot change password
-        vwoL.setPassword("hack123", false);
-    }
-}
-
-// ✅ GOOD DESIGN: Data is PRIVATE
+// Data is PRIVATE
 class GoodVWOLogin {
 
     // PRIVATE variables → data hiding
@@ -63,5 +38,30 @@ class GoodVWOLogin {
         } else {
             System.out.println("Not allowed to change password");
         }
+    }
+}
+
+public class P137_Encap_Demo {
+    public static void main(String[] args) {
+
+        // Creating object of secure login class
+        GoodVWOLogin vwoL = new GoodVWOLogin("admin", "pass123");
+
+        // ❌ Direct access NOT allowed (password is private)
+        // System.out.println(vwoL.password);
+
+        // ✔ Username can be changed via setter
+        vwoL.setUsername("Anuj");
+        System.out.println(vwoL.getUsername());
+
+        // ✔ Password can be read ONLY using getter
+        System.out.println(vwoL.getPassword());
+
+        // ✔ Only admin can change password
+        vwoL.setPassword("12345", true);
+        System.out.println(vwoL.getPassword());
+
+        // ❌ Non-admin cannot change password
+        vwoL.setPassword("hack123", false);
     }
 }
