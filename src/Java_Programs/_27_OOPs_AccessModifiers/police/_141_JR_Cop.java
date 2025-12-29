d332d698f77f9183354e115266b4033a4f1f915d
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-29 16:33:36 +0530

    Update _141_JR_Cop.java

--- src/Java_Programs/_27_OOPs_AccessModifiers/police/_141_JR_Cop.java
+++ src/Java_Programs/_27_OOPs_AccessModifiers/police/_141_JR_Cop.java
@@ -2,7 +2,13 @@ package Java_Programs._27_OOPs_AccessModifiers.police;
 
 public class _141_JR_Cop {
     public static void main(String[] args) {
+
+        // Junior cop creating cop object
         _140_Cop jrCop = new _140_Cop(5);
+
+        // ✔ Allowed because:
+        // - Same package
+        // - Method is PROTECTED
         jrCop.canIShoot();
     }
 }
