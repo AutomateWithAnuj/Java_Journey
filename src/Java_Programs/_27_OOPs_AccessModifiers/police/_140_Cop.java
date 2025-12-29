8fce3074ade5751c9c0798db22acc129d69e7510
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-29 16:33:24 +0530

    Update _140_Cop.java

--- src/Java_Programs/_27_OOPs_AccessModifiers/police/_140_Cop.java
+++ src/Java_Programs/_27_OOPs_AccessModifiers/police/_140_Cop.java
@@ -1,15 +1,27 @@
 package Java_Programs._27_OOPs_AccessModifiers.police;
 
 public class _140_Cop {
+
+    // PUBLIC variable → accessible everywhere
     public int gun;
+
+    // DEFAULT variable → accessible only within same package
     String iCard;
 
-    //Parameterized Constructor
-    public _140_Cop(int bullet){
+    // Parameterized constructor
+    public _140_Cop(int bullet) {
         this.gun = bullet;
     }
-    //Method //Behaviour
-    protected void canIShoot(){
+
+    /*
+     * PROTECTED method
+     * Accessible:
+     * 1. Same package
+     * 2. Subclass (even in different package)
+     * NOT accessible:
+     * - Normal class from another package
+     */
+    protected void canIShoot() {
         System.out.println("Yes you can shoot!");
     }
 }
