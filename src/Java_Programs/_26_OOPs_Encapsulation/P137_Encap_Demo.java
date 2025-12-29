7556bce442875093de16b57178a00ca8bdfe80bf
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-29 15:49:51 +0530

    Update P137_Encap_Demo.java

--- src/Java_Programs/_26_OOPs_Encapsulation/P137_Encap_Demo.java
+++ src/Java_Programs/_26_OOPs_Encapsulation/P137_Encap_Demo.java
@@ -2,58 +2,36 @@ package Java_Programs._26_OOPs_Encapsulation;
 
 public class P137_Encap_Demo {
     public static void main(String[] args) {
-        VWOLogin vwoLogin = new VWOLogin("admin", "pass123");
-        System.out.println(vwoLogin.password); //he can see the password
-        vwoLogin.password = "pass345"; //he can change the password
-        System.out.println(vwoLogin.password); //he has changed the password also
 
-        //as this is terrible if stranger can see the password and change
-        //let us see now a good example with new class that is GoodVWOLogin
-        GoodVWOLogin vwoL = new GoodVWOLogin("admin","pass123");
-        //System.out.println(vwoL.password); //not able to access the password only accessible in that class only
-        //so for changing the ID password we can use the getter setters and change also else no option
+        // Creating object of secure login class
+        GoodVWOLogin vwoL = new GoodVWOLogin("admin", "pass123");
+
+        // ❌ Direct access NOT allowed (password is private)
+        // System.out.println(vwoL.password);
+
+        // ✔ Username can be changed via setter
         vwoL.setUsername("Anuj");
         System.out.println(vwoL.getUsername());
+
+        // ✔ Password can be read ONLY using getter
         System.out.println(vwoL.getPassword());
-        vwoL.setPassword("12345",true);//if you are admin then only you can change the password
+
+        // ✔ Only admin can change password
+        vwoL.setPassword("12345", true);
         System.out.println(vwoL.getPassword());
+
+        // ❌ Non-admin cannot change password
+        vwoL.setPassword("hack123", false);
     }
 }
-class VWOLogin{
-    public String username;
-    public String password;
 
-    VWOLogin(String usr, String pwd){
-        this.username = usr;
-        this.password = pwd;
-    }
-}
-class GoodVWOLogin{
-    private String username; //private means they can only be access within this class
-    private String password; //private means they can only be access within this class
+// ✅ GOOD DESIGN: Data is PRIVATE
+class GoodVWOLogin {
 
-    GoodVWOLogin(String usr, String pwd){
-        this.username = usr;
-        this.password = pwd;
-    }
+    // PRIVATE variables → data hiding
+    private String username;
+    private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    //we don't have to change the password by anyone only admin can do that
-    public void setPassword(String password,boolean isAdmin) {
-        if (isAdmin){
-            this.password = password;
-        } else{
-            System.out.println("Not allowed");
-        }
-    }
-}
+    // Constructor initializes data safely
+    GoodVWOLogin(String usr, String pwd) {
+        this.us
