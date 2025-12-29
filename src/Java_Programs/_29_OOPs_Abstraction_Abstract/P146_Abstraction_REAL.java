b4c92b25731931b14d8186edff87ef56c34fc36f
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-30 00:13:08 +0530

    Update P146_Abstraction_REAL.java

--- src/Java_Programs/_29_OOPs_Abstraction_Abstract/P146_Abstraction_REAL.java
+++ src/Java_Programs/_29_OOPs_Abstraction_Abstract/P146_Abstraction_REAL.java
@@ -1,32 +1,92 @@
 package Java_Programs._29_OOPs_Abstraction_Abstract;
 
-public class P146_Abstraction_REAL {
-}
-abstract class Employee{
+/*
+ * Abstract class Employee
+ *
+ * - Represents a generic employee
+ * - Contains common properties and behaviors
+ * - Cannot be instantiated
+ */
+abstract class Employee {
+
+    // Encapsulated fields
     private String name;
     private String address;
     private int number;
 
-    Employee(){ //it can have default constructors
-
+    // Default constructor
+    Employee() {
     }
 
-    public Employee(String name, String address, int number) {//it can have the parameterized constructor also
+    // Parameterized constructor
+    public Employee(String name, String address, int number) {
         this.name = name;
         this.address = address;
         this.number = number;
     }
 
-    abstract double computePay(); //we can have a incomplete method
+    /*
+     * Abstract method:
+     * - Salary calculation differs for each employee type
+     * - So implementation is deferred to child classes
+     */
+    abstract double computePay();
 
-    void mainCheck(){ //we can also have a complete method
-        System.out.println("Mailing a check to"+this.name+" "+this.address);
+    /*
+     * Concrete method:
+     * - Common behavior for all employees
+     */
+    void mainCheck() {
+        System.out.println("Mailing a check to " + name + " at " + address);
     }
 }
 
-class C1 extends Employee{
+/*
+ * Concrete class C1
+ *
+ * - Extends Employee
+ * - Must implement computePay()
+ */
+class C1 extends Employee {
+
+    // Constructor calling parent constructor
+    public C1(String name, String address, int number) {
+        super(name, address, number);
+    }
+
     @Override
-    double computePay(){
-        return 0.0;
+    double computePay() {
+        // Sample salary calculation
+        return 50000.0;
     }
 }
+
+
+/*
+ * This program demonstrates ABSTRACTION using a real-world example.
+ *
+ * Scenario:
+ * - Employee is a general concept
+ * - Salary calculation differs for different employee types
+ * - So, computePay() is kept abstract
+ */
+
+public class P146_Abstraction_REAL {
+
+    public static void main(String[] args) {
+
+        /*
+         * Creating reference of abstract class Employee
+         * and object of concrete child class C1
+         */
+        Employee emp = new C1("Anuj", "Delhi", 101);
+
+        // Calling concrete method from abstract class
+        emp.mainCheck();
+
+        // Calling implemented abstract method
+        double salary = emp.computePay();
+        System.out.println("Salary = " + salary);
+    }
+}
+
