6f8a026a97991580b5fcf8c57196693ea476a62e
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-29 23:24:33 +0530

    Update P144_OOPs_Abstraction.java

--- src/Java_Programs/_29_OOPs_Abstraction_Abstract/P144_OOPs_Abstraction.java
+++ src/Java_Programs/_29_OOPs_Abstraction_Abstract/P144_OOPs_Abstraction.java
@@ -1,23 +1,74 @@
 package Java_Programs._29_OOPs_Abstraction_Abstract;
 
+/*
+ * This program demonstrates ABSTRACTION using an ABSTRACT CLASS.
+ *
+ * Abstraction means:
+ * - Hiding the implementation details
+ * - Showing only the essential features to the user
+ */
+
 public class P144_OOPs_Abstraction {
+
     public static void main(String[] args) {
-        Child c = new Child();
-        c.loan50k();
-        c.loan25k();
-        //Father f1 = new Father();
+
+        // Creating object of Child class
+        Child child = new Child();
+
+        // Calling implemented abstract method
+        child.loan50k();
+
+        // Calling concrete method inherited from abstract class
+        child.loan25k();
+
+        // Abstract class cannot be instantiated
+        // Father f1 = new Father();  ❌ Not allowed
+
+        /*
+         * Runtime Polymorphism:
+         * Reference of abstract class (Father)
+         * Object of child class (Child)
+         */
         Father f2 = new Child();
+        f2.loan50k();   // Calls Child's implementation
+        f2.loan25k();   // Calls Father's method
     }
 }
-abstract class Father{
+
+/*
+ * Abstract class representing a Parent (Father)
+ * It contains:
+ * - One abstract method (without body)
+ * - One concrete method (with body)
+ */
+abstract class Father {
+
+    /*
+     * Abstract method:
+     * - No implementation
+     * - Forces child class to provide implementation
+     */
     abstract void loan50k();
-    void loan25k(){
-        System.out.println("Given 25k");
+
+    /*
+     * Concrete method:
+     * - Has implementation
+     * - Can be directly used by child class
+     */
+    void loan25k() {
+        System.out.println("Father has given 25k loan");
     }
 }
-class Child extends Father{
+
+/*
+ * Child class extends Father
+ * It MUST implement all abstract methods of Father
+ */
+class Child extends Father {
+
+    // Providing implementation of abstract method
     @Override
-    void loan50k(){
-        System.out.println("Child will pay the loan!");
+    void loan50k() {
+        System.out.println("Child will repay the 50k loan");
     }
 }
