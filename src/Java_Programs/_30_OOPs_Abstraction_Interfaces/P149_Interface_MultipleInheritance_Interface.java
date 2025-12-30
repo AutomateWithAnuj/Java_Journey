7361f16cbd9dba339a5df937feb5827c7f0e8f9b
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-30 16:56:01 +0530

    Update P149_Interface_MultipleInheritance_Interface.java

--- src/Java_Programs/_30_OOPs_Abstraction_Interfaces/P149_Interface_MultipleInheritance_Interface.java
+++ src/Java_Programs/_30_OOPs_Abstraction_Interfaces/P149_Interface_MultipleInheritance_Interface.java
@@ -1,17 +1,60 @@
 package Java_Programs._30_OOPs_Abstraction_Interfaces;
 
-public class P149_Interface_MultipleInheritance_Interface {
+/*
+ * Mother interface.
+ * Defines a rule that any implementing class must provide money().
+ */
+interface Mother {
 
-}
-interface Mother{
+    // Abstract method (public and abstract by default)
     void money();
 }
-interface Father{
+
+/*
+ * Father interface.
+ * Also defines a method with the same signature.
+ */
+interface Father {
+
+    // Abstract method (public and abstract by default)
     void money();
 }
-class Child implements Mother,Father{
+
+/*
+ * Child class implements both Mother and Father interfaces.
+ * This demonstrates multiple inheritance using interfaces.
+ */
+class Child implements Mother, Father {
+
+    /*
+     * Single implementation of money() method.
+     * This method satisfies both Mother and Father interfaces.
+     * Hence, there is NO ambiguity.
+     */
     @Override
-    public void money(){
+    public void money() {
         System.out.println("Everything is yours");
+    }
+}
+
+/*
+ * This class contains the main method.
+ * Program execution starts from here.
+ */
+public class P149_Interface_MultipleInheritance {
+
+    public static void main(String[] args) {
+
+        // Creating an object of the Child class
+        Child child = new Child();
+
+        // Calling money() method
+        // Single implementation resolves conflict from both interfaces
+        child.money();
+
+        /*
+         * Expected Output:
+         * Everything is yours
+         */
     }
 }
