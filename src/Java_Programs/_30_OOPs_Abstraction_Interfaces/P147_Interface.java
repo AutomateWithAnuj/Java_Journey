f186a74df8b6331f56bebe6936910cdd522bb29d
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-30 15:14:34 +0530

    Update P147_Interface.java

--- src/Java_Programs/_30_OOPs_Abstraction_Interfaces/P147_Interface.java
+++ src/Java_Programs/_30_OOPs_Abstraction_Interfaces/P147_Interface.java
@@ -1,26 +1,82 @@
 package Java_Programs._30_OOPs_Abstraction_Interfaces;
 
+/*
+ * This class contains the main method.
+ * Execution of the program starts from here.
+ */
 public class P147_Interface {
-    public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
-        r1.getArea(10,10);
 
-        triangle t1 = new triangle();
-        t1.getArea(3,4);
+    public static void main(String[] args) {
+
+        // Creating object of Rectangle class
+        // Reference type is the implementing class
+        Rectangle rectangle = new Rectangle();
+
+        // Calling getArea() method for rectangle
+        // length = 10, breadth = 10
+        rectangle.getArea(10, 10);
+        // Expected Output:
+        // The area of Rectangle is: 100
+
+
+        // Creating object of Triangle class
+        Triangle triangle = new Triangle();
+
+        // Calling getArea() method for triangle
+        // base = 3, height = 4
+        triangle.getArea(3, 4);
+        // Expected Output:
+        // The area of Triangle is: 6.0
     }
 }
-interface Polygon{
-    void getArea(int length, int breadth);
+
+/*
+ * Polygon is an interface.
+ * It defines a contract for all polygon shapes.
+ * Any class that implements this interface MUST implement getArea().
+ */
+interface Polygon {
+
+    /*
+     * Abstract method to calculate area.
+     * By default, this method is:
+     * public and abstract
+     */
+    void getArea(int value1, int value2);
 }
-class Rectangle implements Polygon{
+
+/*
+ * Rectangle class implements Polygon interface.
+ * It provides its own implementation of getArea().
+ */
+class Rectangle implements Polygon {
+
+    /*
+     * Overriding the getArea() method for Rectangle.
+     * Formula used:
+     * Area = length * breadth
+     */
     @Override
-    public void getArea(int length, int breadth){
-        System.out.println("The area of rectangle is: "+(length*breadth));
+    public void getArea(int length, int breadth) {
+        int area = length * breadth;
+        System.out.println("The area of Rectangle is: " + area);
     }
 }
-class triangle implements Polygon{
+
+/*
+ * Triangle class implements Polygon interface.
+ * It also provides its own implementation of getArea().
+ */
+class Triangle implements Polygon {
+
+    /*
+     * Overriding the getArea() method for Triangle.
+     * Formula used:
+     * Area = 1/2 * base * height
+     */
     @Override
-    public void getArea(int a,int b){
-        System.out.println("The area of triangle is: "+(0.5*a*b*Math.sin(90)));
+    public void getArea(int base, int height) {
+        double area = 0.5 * base * height;
+        System.out.println("The area of Triangle is: " + area);
     }
 }
