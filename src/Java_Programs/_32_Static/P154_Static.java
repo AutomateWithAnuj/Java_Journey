2766de3242b040f8a4cc6a6f805531485ea0f0f0
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2025-12-31 20:59:49 +0530

    Update P154_Static.java

--- src/Java_Programs/_32_Static/P154_Static.java
+++ src/Java_Programs/_32_Static/P154_Static.java
@@ -1,24 +1,58 @@
 package Java_Programs._32_Static;
 
-public class P154_Static {
-    public static void main(String[] args) {
-        B.commonToAll(); //As this is static it can be called without the
-        //B.dispaly(); Not possible because it must be static
-        System.out.println(B.b);
+/**
+ * Demonstrates:
+ * - Static Initialisation Block (SIB)
+ * - Static methods and variables
+ * - Why non-static members cannot be accessed directly in static context
+ */
+
+/**
+ * Class B contains:
+ * - Static Initialization Block
+ * - Instance variable
+ * - Static variable
+ * - Static and non-static methods
+ */
+class B {
+
+    // Static Initialization Block
+    // Executes only once when the class is loaded into memory
+    static {
+        System.out.println("SIB, called once, class is loaded"); // Printed once
+    }
+
+    int a = 10;          // Instance variable (belongs to object)
+    static int b = 10;   // Static variable (shared by all objects)
+
+    // Non-static method (requires object to call)
+    void display() {
+        System.out.println("Non Static"); // Printed when called using an object
+    }
+
+    // Static method (can be called using class name)
+    static void commonToAll() {
+
+        // System.out.println(a); 
+        // Not allowed: instance variable cannot be accessed in static method
+
+        System.out.println(b);       // Output: 10 (static variable access allowed)
+        System.out.println("Static"); // Output: Static
     }
 }
-class B{
-    static {
-        System.out.println("SIB, called once, class is loaded");
-    }
-    int a = 10;
-    static int b= 10;
-    void display(){
-        System.out.println("Non Static");
-    }
-    static void commonToAll(){
-        //System.out.println(a); //not possible because instance variable cannot be called inside static method
-        System.out.println(b);
-        System.out.println("Static");
+
+public class P154_Static {
+
+    public static void main(String[] args) {
+
+        // Calling static method using class name (no object required)
+        B.commonToAll();
+        // Output:
+        // SIB, called once, class is loaded   (from static block)
+        // 10                                 (static variable b)
+        // Static                             (from static method)
+
+        // Printing static variable using class name (best practice)
+        System.out.println(B.b); // Output: 10
     }
 }
