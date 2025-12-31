9a5a92b0c346f910a116edbc2999047bb80a2276
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-01 00:46:42 +0530

    Update P158_ENUM_ProjectURLs.java

--- src/Java_Programs/_33_Enum/P158_ENUM_ProjectURLs.java
+++ src/Java_Programs/_33_Enum/P158_ENUM_ProjectURLs.java
@@ -1,10 +1,29 @@
 package Java_Programs._33_Enum;
 
-public class P158_ENUM_ProjectURLs {
-    public static void main(String[] args) {
-        System.out.println(Project_URLs.katalon);
-    }
+/*
+ * This program demonstrates the use of ENUM
+ * to store fixed project-related constant values.
+ */
+
+/*
+ * ENUM is used to define a fixed set of constants.
+ * These values are:
+ * - Type-safe
+ * - Read-only (cannot be modified)
+ * - Easy to use and maintain
+ */
+enum ProjectUrls {
+    GOOGLE,
+    REST_ASSURED,
+    KATALON,
+    VWO
 }
-enum Project_URLs{
-    google, rest_assured, katalon, vwo
+
+public class P158_ENUM_ProjectURLs {
+
+    public static void main(String[] args) {
+
+        // Printing a constant value from ENUM
+        System.out.println(ProjectUrls.KATALON); // Output: KATALON
+    }
 }
