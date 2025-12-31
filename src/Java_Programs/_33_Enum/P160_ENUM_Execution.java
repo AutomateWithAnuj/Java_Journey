79a2f9ade73e7cc8837189a6ec94394c59ad1a4f
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-01 00:50:26 +0530

    Update P160_ENUM_Execution.java

--- src/Java_Programs/_33_Enum/P160_ENUM_Execution.java
+++ src/Java_Programs/_33_Enum/P160_ENUM_Execution.java
@@ -1,12 +1,23 @@
 package Java_Programs._33_Enum;
 
-import java.awt.*;
-
+/*
+ * This class demonstrates how ENUM values are accessed
+ * and used during execution.
+ */
 public class P160_ENUM_Execution {
-    public static void main(String[] args) {
-        System.out.println(P159_ENUM_Locators.page_button.getLocators());
-        System.out.println(P159_ENUM_Locators.page_input.getLocators());
 
+    public static void main(String[] args) {
+
+        // Accessing locator value stored inside ENUM constant
+        System.out.println(P159_ENUM_Locators.page_button.getLocators());
+        // Output: #btn
+
+        // Accessing another locator value from ENUM
+        System.out.println(P159_ENUM_Locators.page_input.getLocators());
+        // Output: #input1
+
+        // Accessing HEX color code from another ENUM
         System.out.println(P161_ENUM_Colors.RED.getHexCode());
+        // Output: (depends on RED hex value defined in P161_ENUM_Colors)
     }
 }
