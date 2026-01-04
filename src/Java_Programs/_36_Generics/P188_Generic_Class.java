3c2c870108effff183a6b439c60e54043db06861
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-04 23:04:05 +0530

    Update P188_Generic_Class.java

--- src/Java_Programs/_36_Generics/P188_Generic_Class.java
+++ src/Java_Programs/_36_Generics/P188_Generic_Class.java
@@ -1,22 +1,53 @@
 package Java_Programs._36_Generics;
 
+/*
+ * This class demonstrates the usage of a **generic class**.
+ * GenericClass<T> can work with any reference data type.
+ */
 public class P188_Generic_Class {
-    public static void main(String[] args) {
-        GenericClass<Integer> intObject = new GenericClass<>(5);
-        GenericClass<String> stringObject = new GenericClass<>("Anuj");
 
-        //T is a placeholder it can have any datatype
+    public static void main(String[] args) {
+
+        // Creating a GenericClass object with Integer type
+        GenericClass<Integer> intObject = new GenericClass<>(5);
+        // intObject holds Integer value 5
+
+        // Creating a GenericClass object with String type
+        GenericClass<String> stringObject = new GenericClass<>("Anuj");
+        // stringObject holds String value "Anuj"
+
+        // T is a placeholder that can represent any reference data type
     }
 }
-class GenericClass<T>{ //here in generic class we have to give generic After classname
+
+/*
+ * Generic class declaration
+ * <T> is defined after the class name and can be used anywhere inside the class
+ */
+class GenericClass<T> {
+
+    // Variable of generic type T
     private T data;
-    public GenericClass(T data){
-        this.data = data;
+
+    /*
+     * Constructor to initialize the generic data
+     */
+    public GenericClass(T data) {
+        this.data = data; // assigns passed value to class variable
     }
-    public void setData(T data){
-        this.data = data;
+
+    /*
+     * Setter method to update data
+     * No need to declare <T> again inside the class
+     */
+    public void setData(T data) {
+        this.data = data; // updates the stored value
     }
-    public T getData(){
-        return this.data;
-    } //so for generic class no need to mention genric again we can use it directly
+
+    /*
+     * Getter method to return data
+     */
+    public T getData() {
+        return this.data; // returns the stored value
+    }
 }
