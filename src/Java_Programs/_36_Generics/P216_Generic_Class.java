312bb958c6e258bedb68546d4fa390fd6d52457c
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-04 23:05:50 +0530

    Update P216_Generic_Class.java

--- src/Java_Programs/_36_Generics/P216_Generic_Class.java
+++ src/Java_Programs/_36_Generics/P216_Generic_Class.java
@@ -1,22 +1,47 @@
 package Java_Programs._36_Generics;
 
+/*
+ * This class demonstrates a **generic class** with different data types.
+ * The same class works for Integer, Double, and String using generics.
+ */
 public class P216_Generic_Class {
-    public static void main(String[] args) {
-        Test<Integer> Integer_T = new Test<Integer>(5);
-        System.out.println(Integer_T.obj);
-        Test<Double> Double_T = new Test<Double>(5.3);
-        System.out.println(Double_T.obj);
-        Test<String> String_T = new Test<String>("Anuj");
-        System.out.println(String_T.obj);
 
+    public static void main(String[] args) {
+
+        // Creating Test object with Integer type
+        Test<Integer> integerObj = new Test<>(5);
+        System.out.println(integerObj.obj); // Output: 5
+
+        // Creating Test object with Double type
+        Test<Double> doubleObj = new Test<>(5.3);
+        System.out.println(doubleObj.obj); // Output: 5.3
+
+        // Creating Test object with String type
+        Test<String> stringObj = new Test<>("Anuj");
+        System.out.println(stringObj.obj); // Output: Anuj
     }
 }
-class Test<T>{ //in this we have given generic before so after in this class no generics will be needed
+
+/*
+ * Generic class declaration
+ * <T> allows this class to work with any reference data type
+ */
+class Test<T> {
+
+    // Variable of generic type T
     T obj;
-    Test(T obj){
-        this.obj = obj;
+
+    /*
+     * Constructor initializes the generic object
+     */
+    Test(T obj) {
+        this.obj = obj; // assigns passed value to class variable
     }
-    public T getObj(){
-        return this.obj;
+
+    /*
+     * Getter method to return the stored object
+     */
+    public T getObj() {
+        return this.obj; // returns the stored value
     }
 }
