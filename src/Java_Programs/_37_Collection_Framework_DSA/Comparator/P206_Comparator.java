9ec3f501f1cee5194d4318da315346e2a21b7b87
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-05 23:34:26 +0530

    Update P206_Comparator.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Comparator/P206_Comparator.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Comparator/P206_Comparator.java
@@ -5,87 +5,118 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Demonstrates sorting custom objects using Comparator
+ * Different sorting logics are defined outside the Student class
+ */
 public class P206_Comparator {
-    public static void main(String[] args) {
-        Student s1 = new Student(101, "Anuj",24);
-        Student s2 = new Student(104, "Amit",55);
-        Student s3 = new Student(103, "Pramod",35);
-       List<Student> sl = new ArrayList<>();
-        sl.add(s1);
-        sl.add(s2);
-        sl.add(s3);
-        //Collections.sort(sl); //not possible to sort them like this
-        //if their had been similar integers they would have been shorted till now
-        //we cannot short them because we have not given a way to sort them for these objects
-        //so we have to implement the comparable
-        Collections.sort(sl,new AgeComparator()); //it will be automatically shorted by this
-        System.out.println("Sort by Age: "+sl);
-        Collections.sort(sl,new NameComparator()); //it will be automatically shorted by this
-        System.out.println("Sort by Name: "+sl);
-        Collections.sort(sl,new RolllNoComparator()); //it will be automatically shorted by this
-        System.out.println("Sort by RollNo: "+sl);
 
+    public static void main(String[] args) {
+
+        // Creating Student objects
+        Student s1 = new Student(101, "Anuj", 24);
+        Student s2 = new Student(104, "Amit", 55);
+        Student s3 = new Student(103, "Pramod", 35);
+
+        // List to store Student objects
+        List<Student> students = new ArrayList<>();
+
+        // Adding students to the list
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+
+        // Sorting by Age using AgeComparator
+        Collections.sort(students, new AgeComparator());
+        System.out.println("Sort by Age: " + students);
+        // Output: Students sorted in ascending order of age
+
+        // Sorting by Name using NameComparator
+        Collections.sort(students, new NameComparator());
+        System.out.println("Sort by Name: " + students);
+        // Output: Students sorted alphabetically by name
+
+        // Sorting by Roll Number using RollNoComparator
+        Collections.sort(students, new RollNoComparator());
+        System.out.println("Sort by RollNo: " + students);
+        // Output: Students sorted in ascending order of roll number
     }
 }
-class Student { //to implement Comparable we have to implement CompareTo
+
+/**
+ * Plain Student class
+ * Does NOT implement Comparable
+ * Sorting logic is provided externally using Comparator
+ */
+class Student {
+
+    // Private fields for encapsulation
     private int rollNo;
     private String name;
     private int age;
 
+    // Constructor to initialize Student object
     public Student(int rollNo, String name, int age) {
         this.rollNo = rollNo;
         this.name = name;
         this.age = age;
     }
 
+    // Getter methods used by Comparator classes
     public int getRollNo() {
         return rollNo;
-    }
-
-    public void setRollNo(int rollNo) {
-        this.rollNo = rollNo;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
+    /**
+     * Used automatically when Student object is printed
+     */
     @Override
     public String toString() {
-        return "Student{"+"rollNo="+rollNo+", name="+name+", age="+age+'}';
-    }
-
-}
-class AgeComparator implements Comparator<Student>{
-
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getAge()-o2.getAge();
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name=" + name +
+                ", age=" + age +
+                '}';
     }
 }
-class NameComparator implements Comparator<Student>{
+
+/**
+ * Comparator to sort Student objects by age
+ */
+class AgeComparator implements Comparator<Student> {
 
     @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getName().compareTo(o2.getName());
+    public int compare(Student s1, Student s2) {
+        return s1.getAge() - s2.getAge(); // ascending order by age
     }
 }
-class RolllNoComparator implements Comparator<Student>{
+
+/**
+ * Comparator to sort Student objects by name
+ */
+class NameComparator implements Comparator<Student> {
 
     @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getRollNo()-o2.getRollNo();
+    public int compare(Student s1, Student s2) {
+        return s1.getName().compareTo(s2.getName()); // alphabetical order
+    }
+}
+
+/**
+ * Comparator to sort Student objects by roll number
+ */
+class RollNoComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.getRollNo() - s2.getRollNo(); // ascending order by roll number
     }
 }
