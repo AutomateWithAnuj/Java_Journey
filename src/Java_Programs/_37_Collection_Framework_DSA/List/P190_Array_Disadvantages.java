3ef72fd4d244f8c8410654a1429b3b70b8ba961b
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-05 23:35:21 +0530

    Update P190_Array_Disadvantages.java

--- src/Java_Programs/_37_Collection_Framework_DSA/List/P190_Array_Disadvantages.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/List/P190_Array_Disadvantages.java
@@ -1,13 +1,39 @@
 package Java_Programs._37_Collection_Framework_DSA.List;
 
+/**
+ * Demonstrates the disadvantages of using Arrays in Java
+ */
 public class P190_Array_Disadvantages {
+
     public static void main(String[] args) {
+
+        // Creating an Integer array with fixed size of 10
         Integer[] arr = new Integer[10];
+
+        // Storing an Integer value at index 0
         arr[0] = 12;
-        //arr[1] = "Anuj"; //Not possible to add different datatype in array
-        //Fixed size problem
-        //Similar Datatypes problem
-        //Doesn't grow automatically problem
-        //if we use fewer elements than it will be wastage of memory let's suppose we use 10 out 100 then 90 will be wasted
+
+        // arr[1] = "Anuj";
+        // ❌ Not allowed: Arrays are type-safe
+        // Only Integer values can be stored in Integer[]
+
+        /*
+         * Disadvantages of Arrays:
+         *
+         * 1. Fixed Size:
+         *    - Size must be defined at creation time
+         *    - Cannot grow or shrink dynamically
+         *
+         * 2. Similar Data Type Only:
+         *    - Cannot store different types of data in a single array
+         *
+         * 3. No Automatic Growth:
+         *    - If more elements are needed, a new array must be created
+         *
+         * 4. Memory Wastage:
+         *    - If array size is large and few elements are used,
+         *      unused memory is wasted
+         *    - Example: size = 100, used = 10 → 90 unused
+         */
     }
 }
