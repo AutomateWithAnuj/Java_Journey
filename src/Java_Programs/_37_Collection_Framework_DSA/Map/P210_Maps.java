d2eac6f84a6bc134920a36508efcfe668a045bf5
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:29:14 +0530

    Update P210_Maps.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Map/P210_Maps.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Map/P210_Maps.java
@@ -5,30 +5,45 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Demonstrates different Map implementations in Java:
+ * HashMap, LinkedHashMap, and TreeMap
+ */
 public class P210_Maps {
+
     public static void main(String[] args) {
-        Map m1 = new HashMap();
-        //keys will be in the different order in which user has entered
-        m1.put("name","Anuj");
-        m1.put("age","24");
-        m1.put("rollNo","1");
-        m1.put("PhoneNo","9171167945");
-        System.out.println(m1);
 
-        Map m2 = new LinkedHashMap();
-        //keys will be in the same order in which user has entered
-        m2.put("name","Anuj");
-        m2.put("age","24");
-        m2.put("rollNo","1");
-        m2.put("PhoneNo","9171167945");
-        System.out.println(m2);
+        // ---------------- HashMap ----------------
+        // Stores key-value pairs without any guaranteed order
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("name", "Anuj");
+        hashMap.put("age", "24");
+        hashMap.put("rollNo", "1");
+        hashMap.put("PhoneNo", "9171167945");
+        System.out.println("HashMap: " + hashMap);
+        // Output example: {rollNo=1, age=24, name=Anuj, PhoneNo=9171167945}
+        // Note: order of keys may vary
 
-        Map m3 = new TreeMap();
-        //pairs will be shorted based on keys
-        m3.put("Name","Anuj");
-        m3.put("Age","24");
-        m3.put("RollNo","1");
-        m3.put("PhoneNo","9171167945");
-        System.out.println(m3);
+        // ---------------- LinkedHashMap ----------------
+        // Maintains insertion order of keys
+        Map<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("name", "Anuj");
+        linkedHashMap.put("age", "24");
+        linkedHashMap.put("rollNo", "1");
+        linkedHashMap.put("PhoneNo", "9171167945");
+        System.out.println("LinkedHashMap: " + linkedHashMap);
+        // Output: {name=Anuj, age=24, rollNo=1, PhoneNo=9171167945}
+        // Keys appear in the order they were added
+
+        // ---------------- TreeMap ----------------
+        // Sorts keys in natural order (alphabetical for Strings)
+        Map<String, String> treeMap = new TreeMap<>();
+        treeMap.put("Name", "Anuj");
+        treeMap.put("Age", "24");
+        treeMap.put("RollNo", "1");
+        treeMap.put("PhoneNo", "9171167945");
+        System.out.println("TreeMap: " + treeMap);
+        // Output: {Age=24, Name=Anuj, PhoneNo=9171167945, RollNo=1}
+        // Keys are sorted alphabetically
     }
 }
