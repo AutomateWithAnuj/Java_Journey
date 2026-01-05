d7acedda72d182fc615c49ac89a897fa8dc8b833
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:22:33 +0530

    Update P200_UserInput_List.java

--- src/Java_Programs/_37_Collection_Framework_DSA/List/P200_UserInput_List.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/List/P200_UserInput_List.java
@@ -4,35 +4,63 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
+/**
+ * Demonstrates taking multiple user inputs
+ * and storing them in ArrayLists
+ */
 public class P200_UserInput_List {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //Multiple inputs from user and store them in separate
-        //names, ages - store them
-        ArrayList<String> names = new ArrayList();
-        ArrayList<Integer> ages = new ArrayList();
-        String continueInput = "Yes"; //Control variable for input loop
-        while (continueInput.equalsIgnoreCase("Yes")){
+
+        // Scanner to take input from user
+        Scanner scanner = new Scanner(System.in);
+
+        // ArrayLists to store names and ages separately
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer> ages = new ArrayList<>();
+
+        // Control variable to continue or stop input loop
+        String continueInput = "Yes";
+
+        // Loop to take multiple inputs
+        while (continueInput.equalsIgnoreCase("Yes")) {
+
+            // Taking name input
             System.out.print("Enter Name: ");
-            String name = sc.next();
+            String name = scanner.next();
+
+            // Taking age input
             System.out.print("Enter Age: ");
-            int age = sc.nextInt();
+            int age = scanner.nextInt();
+
+            // Storing inputs into respective lists
             names.add(name);
             ages.add(age);
-            System.out.print("Do you want to input more names & their age(Yes/No): ");
-            continueInput = sc.next();
+
+            // Asking user whether to continue
+            System.out.print("Do you want to input more names & their age (Yes/No): ");
+            continueInput = scanner.next();
         }
+
+        // Printing names list using Iterator
         System.out.print("Your Name Array is: ");
-        Iterator iterator = names.iterator();
-        while (iterator.hasNext()){
-            System.out.print(iterator.next()+" ");
+        Iterator<String> nameIterator = names.iterator();
+        while (nameIterator.hasNext()) {
+            System.out.print(nameIterator.next() + " ");
+            // Output example: Anuj Amit Rahul
         }
-        System.out.println();
+
+        System.out.println(); // Line break for better output formatting
+
+        // Printing ages list using Iterator
         System.out.print("Your Ages Array is: ");
-        Iterator iterator2 = ages.iterator();
-        while (iterator2.hasNext()){
-            System.out.print(iterator2.next()+" ");
+        Iterator<Integer> ageIterator = ages.iterator();
+        while (ageIterator.hasNext()) {
+            System.out.print(ageIterator.next() + " ");
+            // Output example: 24 30 28
         }
-        sc.close();
+
+        // Closing scanner resource
+        scanner.close();
     }
 }
