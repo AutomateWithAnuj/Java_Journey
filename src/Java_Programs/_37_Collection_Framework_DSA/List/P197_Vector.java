43ba7564606f2a4f54c7a1e2249dfa4655d93457
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:06:36 +0530

    Update P197_Vector.java

--- src/Java_Programs/_37_Collection_Framework_DSA/List/P197_Vector.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/List/P197_Vector.java
@@ -5,48 +5,78 @@ import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Vector;
 
+/**
+ * Demonstrates usage of Vector and different ways to iterate over it
+ */
 public class P197_Vector {
+
     public static void main(String[] args) {
-        Vector v = new Vector();
-        Vector<Integer> v2 = new Vector(10);//here we can give the initial capacity
-        //we can add elements in this like the ArrayList & LinkedList
-        v.add("Anuj");
-        v.add("Rajput");
-        v.add("Amit");
-        System.out.println(v);
-        System.out.println(v.contains("Amit"));
-        System.out.println(v);
 
+        // Creating a Vector (non-generic, can store any type of object)
+        Vector vector = new Vector();
+
+        // Creating a generic Vector with initial capacity of 10
+        Vector<Integer> integerVector = new Vector<>(10); // capacity can be specified in Vector
+
+        // Adding elements to the Vector
+        vector.add("Anuj");
+        vector.add("Rajput");
+        vector.add("Amit");
+
+        // Printing the Vector
+        System.out.println(vector);
+        // Output: [Anuj, Rajput, Amit]
+
+        // Checking if Vector contains a specific element
+        System.out.println(vector.contains("Amit"));
+        // Output: true
+
+        System.out.println(vector); // Prints the same Vector again
+
+        // -------- Traversal using For Loop --------
         System.out.println("----- For Loop -----");
-        for (int i=0;i<v.size();i++){
-            System.out.print(v.get(i)+" ");
+        for (int i = 0; i < vector.size(); i++) {
+            System.out.print(vector.get(i) + " ");
+            // Output: Anuj Rajput Amit
         }
-        System.out.println("----- For Each Loop -----");
-        for (Object o:v){ //it can contain any kind of datatypes so we have to consider it as object
-            System.out.print(o+" ");
+
+        // -------- Traversal using For-Each Loop --------
+        System.out.println("\n----- For Each Loop -----");
+        for (Object obj : vector) { // Object used because Vector is non-generic
+            System.out.print(obj + " ");
+            // Output: Anuj Rajput Amit
         }
-        System.out.println("----- Iterator -----");
-        Iterator iterator = v.iterator();
-        while (iterator.hasNext()){
+
+        // -------- Traversal using Iterator --------
+        System.out.println("\n----- Iterator -----");
+        Iterator iterator = vector.iterator();
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
+            // Output (each on new line): Anuj, Rajput, Amit
         }
 
+        // -------- Traversal using Enumeration (Legacy Cursor) --------
         System.out.println("---- Enumeration -----");
-        Enumeration<Object> enumeration = v.elements();
-        while (enumeration.hasMoreElements()){
+        Enumeration<Object> enumeration = vector.elements();
+        while (enumeration.hasMoreElements()) {
             System.out.println(enumeration.nextElement());
+            // Output (each on new line): Anuj, Rajput, Amit
         }
 
-        System.out.println("---- ListIterator -----");
-        ListIterator listIterator = v.listIterator();
-        while (listIterator.hasNext()){
+        // -------- Traversal using ListIterator (Forward) --------
+        System.out.println("---- ListIterator (Forward) -----");
+        ListIterator listIterator = vector.listIterator();
+        while (listIterator.hasNext()) {
             System.out.println(listIterator.next());
+            // Output (each on new line): Anuj, Rajput, Amit
         }
 
-        System.out.println("---- Backward ListIterator -----");
-        ListIterator listIterator2 = v.listIterator();
-        while (listIterator2.hasPrevious()){
-            System.out.println(listIterator2.previous());
+        // -------- Traversal using ListIterator (Backward) --------
+        System.out.println("---- ListIterator (Backward) -----");
+        ListIterator backwardIterator = vector.listIterator(vector.size());
+        while (backwardIterator.hasPrevious()) {
+            System.out.println(backwardIterator.previous());
+            // Output (each on new line): Amit, Rajput, Anuj
         }
     }
 }
