17b7229779f947b7508443d156778ad6bd4d7e0b
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:31:01 +0530

    Update P213_Iterate_Over_Map.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Map/P213_Iterate_Over_Map.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Map/P213_Iterate_Over_Map.java
@@ -1,20 +1,35 @@
 package Java_Programs._37_Collection_Framework_DSA.Map;
 
 import java.util.HashMap;
-import java.util.*;
+import java.util.Map;
 
+/**
+ * Demonstrates how to iterate over a Map using an enhanced for-loop
+ * and the entrySet() method.
+ */
 public class P213_Iterate_Over_Map {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap();
-        map.put("id1",1);
-        map.put("id2",2);
-        map.put("id3",3);
-        map.put("id4",null);
-        map.put("id5",null);
-        map.put("id6",100);
-        //we have to use the for advance loop and this is little complex so we have to learn this
-        for (Map.Entry<String,Integer> item: map.entrySet()){
-            System.out.println(item.getKey()+ " -> "+item.getValue());
+        // Create a HashMap with String keys and Integer values
+        Map<String, Integer> map = new HashMap<>();
+        map.put("id1", 1);     // Add key-value pair
+        map.put("id2", 2);
+        map.put("id3", 3);
+        map.put("id4", null);  // Null values are allowed
+        map.put("id5", null);
+        map.put("id6", 100);
+
+        // Iterate over the map using entrySet() and enhanced for-loop
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            // Print key and value of each map entry
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
+
+        // Example Output (order may vary due to HashMap):
+        // id1 -> 1
+        // id2 -> 2
+        // id3 -> 3
+        // id4 -> null
+        // id5 -> null
+        // id6 -> 100
     }
 }
