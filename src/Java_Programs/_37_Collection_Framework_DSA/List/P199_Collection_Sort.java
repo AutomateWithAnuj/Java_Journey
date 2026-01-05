d05294a07ac1cdb7b5e5c8fa3c1da84a461e54be
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:21:56 +0530

    Update P199_Collection_Sort.java

--- src/Java_Programs/_37_Collection_Framework_DSA/List/P199_Collection_Sort.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/List/P199_Collection_Sort.java
@@ -4,21 +4,34 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Demonstrates sorting of a List using Collections class
+ */
 public class P199_Collection_Sort {
+
     public static void main(String[] args) {
+
+        // Creating a non-generic List to store marks
         List marks = new ArrayList();
+
+        // Adding integer values (marks)
         marks.add(91);
         marks.add(74);
         marks.add(83);
         marks.add(56);
-        System.out.println(marks);
-        //sorting in ascending order
-        Collections.sort(marks); //there is a class with name of collections
-        System.out.println(marks);
 
-        //sorting in decending order
-        Collections.sort(marks,Collections.reverseOrder()); //there is a class with name of collections
+        // Printing original list
         System.out.println(marks);
+        // Output: [91, 74, 83, 56]
 
+        // Sorting list in ascending order
+        Collections.sort(marks); // Uses natural ordering of elements
+        System.out.println(marks);
+        // Output: [56, 74, 83, 91]
+
+        // Sorting list in descending order
+        Collections.sort(marks, Collections.reverseOrder());
+        System.out.println(marks);
+        // Output: [91, 83, 74, 56]
     }
 }
