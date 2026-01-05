14ff975f334d58d80a003618d1d7fb899f01f287
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-06 00:38:39 +0530

    Update P208_LinkedList.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Queue/P208_LinkedList.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Queue/P208_LinkedList.java
@@ -3,19 +3,25 @@ package Java_Programs._37_Collection_Framework_DSA.Queue;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
 
+/**
+ * Demonstrates the usage of a LinkedList as a Queue.
+ * LinkedList maintains insertion order and implements Queue interface.
+ */
 public class P208_LinkedList {
     public static void main(String[] args) {
+        // Create a Queue using LinkedList (FIFO order)
         Queue<Integer> queue = new LinkedList<>();
+
+        // Add elements to the queue
         queue.add(6);
         queue.add(1);
         queue.add(8);
-        //it is the kind of queue which will keep the order
-        //this is the most used queue and sometimes used in queue more than types of queue
+
+        // Iterate over the queue using an Iterator
         Iterator<Integer> iterator = queue.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next()); // Output: 6, 1, 8 (in insertion order)
         }
     }
 }
