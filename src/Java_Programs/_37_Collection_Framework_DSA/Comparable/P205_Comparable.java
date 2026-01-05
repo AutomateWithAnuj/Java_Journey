8eb3e681b3b1c0c12b6d0b729cb48e45d151a35a
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-01-05 23:33:12 +0530

    Update P205_Comparable.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Comparable/P205_Comparable.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Comparable/P205_Comparable.java
@@ -4,45 +4,77 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Demonstrates sorting custom objects using Comparable
+ */
 public class P205_Comparable {
+
     public static void main(String[] args) {
-        Student s1 = new Student(101, "Anuj",24);
-        Student s2 = new Student(104, "Amit",55);
-        Student s3 = new Student(103, "Pramod",35);
-        List<Student> sl = new ArrayList();
-        sl.add(s1);
-        sl.add(s2);
-        sl.add(s3);
-        //Collections.sort(sl); //not possible to sort them like this
-        //if their had been similar integers they would have been shorted till now
-        //we cannot short them because we have not given a way to sort them for these objects
-        //so we have to implement the comparable
-        Collections.sort(sl); //it will be automatically shorted by this
-        System.out.println(sl);
+
+        // Creating Student objects
+        Student s1 = new Student(101, "Anuj", 24);
+        Student s2 = new Student(104, "Amit", 55);
+        Student s3 = new Student(103, "Pramod", 35);
+
+        // List to store Student objects (type-safe using generics)
+        List<Student> students = new ArrayList<>();
+
+        // Adding objects to the list
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+
+        // Sorting the list
+        // Works because Student implements Comparable<Student>
+        Collections.sort(students);
+
+        // Printing sorted list
+        // Output will be sorted based on name (Amit, Anuj, Pramod)
+        System.out.println(students); // [Student{rollNo=104, name=Amit, age=55}, ...]
     }
 }
-class Student implements Comparable<Student>{ //to implement Comparable we have to implement CompareTo
+
+/**
+ * Student class implementing Comparable to define natural sorting order
+ */
+class Student implements Comparable<Student> {
+
+    // Instance variables (kept private for encapsulation)
     private int rollNo;
     private String name;
     private int age;
 
+    // Constructor to initialize Student object
     public Student(int rollNo, String name, int age) {
         this.rollNo = rollNo;
         this.name = name;
         this.age = age;
     }
 
+    /**
+     * Used automatically when object is printed
+     */
     @Override
     public String toString() {
-        return "Student{"+"rollNo="+rollNo+", name="+name+", age="+age+'}';
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name=" + name +
+                ", age=" + age +
+                '}';
     }
 
+    /**
+     * Defines how two Student objects should be compared
+     * Current logic: sort by name in ascending order
+     */
     @Override
-    public int compareTo(Student o) {
-        //return this.rollNo-o.rollNo; //by this we can short them by the age
-        //return this.age-o.age; //by this they will be shorted by age
-        //we can also do with name but for string we have to compare like this because this will not work return this.name-o.name;
-        return this.name.compareTo(o.name);
-        //but we cannot do for all at a time we can do sort them with only 1 at a time
+    public int compareTo(Student other) {
+
+        // String comparison using compareTo (lexicographical order)
+        return this.name.compareTo(other.name);
+
+        // Other valid options (only one at a time):
+        // return this.rollNo - other.rollNo; // sort by roll number
+        // return this.age - other.age;       // sort by age
     }
 }
