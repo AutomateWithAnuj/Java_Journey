9b9e599b20efee9ada77fadf966e1686f56e6d44
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-03-31 12:18:32 +0530

    Update CharFrequency.java

--- src/Java_Programs/_37_Collection_Framework_DSA/Map/CharFrequency.java
+++ src/Java_Programs/_37_Collection_Framework_DSA/Map/CharFrequency.java
@@ -4,24 +4,40 @@ import java.util.Map;
 public class Test {
     public static void main(String[] args) {
 
+        // Input string
         String name = "anuj rajput";
 
+        // Create a Map to store character and its count
+        // Character = key (a, n, u, j...)
+        // Integer = value (count)
         Map<Character, Integer> map = new HashMap<>();
 
+        // Convert string into character array and loop through each character
         for (char c : name.toCharArray()) {
 
-            if (c != ' ') { // ignore space
+            // Ignore spaces
+            if (c != ' ') { 
 
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
+                // Check if character already exists in map
+                if (map.containsKey(c)) { //c does not exist as of now in start
+                    
+                    // If exists, get current count and increase by 1
+                    map.put(c, map.get(c) + 1); //fetching its current value
+                    
                 } else {
+                    
+                    // If character appears first time, add it with count = 1
                     map.put(c, 1);
                 }
             }
         }
 
-        // Print result
+        // Loop through map to print key and value
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            
+            // entry.getKey() -> character
+            // entry.getValue() -> count
+            
             System.out.print(entry.getKey() + "" + entry.getValue());
         }
     }
